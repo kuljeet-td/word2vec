4d49b5b8e2946a14300a4289d8fa518788948555
kuljeet-td <kuljeet.s@tatrasdata.com>
2023-02-28 11:52:11 +0530

    update

--- word2vec/api/endpoints.py
+++ word2vec/api/endpoints.py
@@ -1,34 +1,35 @@
-from gensim.models import KeyedVectors
+import logging
+
+import numpy as np
 from flask import request, jsonify, Blueprint
+from gensim.models import KeyedVectors
 
 word2vec = Blueprint("w2v_app", __name__)
+logging.getLogger().setLevel(logging.INFO)
 
 
-def load_w2v_model(path="word2vec/data"):
+def load_w2v_model(path="/data"):
     """
     load the data files
     """
-    filename = '{}/GoogleNews-vectors-negative300.bin.gz'.format(path)
+    filename = f'{path}/GoogleNews-vectors-negative300.bin.gz'
     model_ = KeyedVectors.load_word2vec_format(filename, binary=True)
     return model_
 
 
 model = load_w2v_model()
+logging.info("Model has been loaded...")
 
 
-@word2vec.route('/word2vec/', methods=['POST'])
+@word2vec.route('/get_vector', methods=['GET'])
 def return_vector():
     try:
-        payload_data_ = request.get_json()
-        word = payload_data_['word']
-        if word in model.vocab:
-            vector = model.get_vector(word=word.strip())
-            return jsonify(
-                {'data': vector.tolist(), 'status_code': 200})
-        else:
-            return jsonify(
-                {'message': '404 Not Found',
-                 'status': '404'}
-            )
+        word = request.args.get('word')
+        arr = np.array([model.get_vector(word=word.strip()) for word in word.split(" ")]).mean(axis=0)
+        if arr.size < 300:
+            return jsonify({'data': np.zeros(shape=(300,)).tolist()}), 200
+        return jsonify({'data': arr.tolist()}), arr
     except Exception as e:
-        return jsonify({'error': str(e)})
+        logging.error(e)
+        return jsonify(
+            {'message': 'Something went wrong, please try again later'}), 500
